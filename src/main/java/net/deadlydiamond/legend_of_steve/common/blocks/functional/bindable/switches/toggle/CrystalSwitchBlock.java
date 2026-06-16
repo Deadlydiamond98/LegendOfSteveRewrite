c284413aa2343d73c1b29fc53427f159d38b8091
deadlydiamond98 <diamonderrick1@gmail.com>
2026-06-16 04:51:50 -0400

    - Switch Block Recipes implemented (though they're using glass as a placeholder for un-implemented block) - Re-added Crystal Switch Ambient Sounds - Dungeon Table Group Binding now handled via tag

--- src/main/java/net/deadlydiamond/legend_of_steve/common/blocks/functional/bindable/switches/toggle/CrystalSwitchBlock.java
+++ src/main/java/net/deadlydiamond/legend_of_steve/common/blocks/functional/bindable/switches/toggle/CrystalSwitchBlock.java
@@ -105,7 +105,7 @@ public class CrystalSwitchBlock extends Block implements Waterloggable, ISwitchB
             createSwitchParticle(world, pos, CrystalSwitchCollisions.ENTIRE_ORB_SHAPE, 0.125f, isOn(world, pos), true);
         }
 
-        if (random.nextInt(750) == 0) {
+        if (random.nextInt(500) == 0) {
             world.playSoundAtBlockCenter(pos, ZeldaSounds.CRYSTAL_SWITCH_AMBIENT, SoundCategory.BLOCKS, 0.5f, 1, true);
         }
     }
